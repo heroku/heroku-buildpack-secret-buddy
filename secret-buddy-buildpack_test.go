75fe2f139424aadf0e175595d2e15484425020a9
jorgelopezdan <jorge.lopez@salesforce.com>
2024-07-21 22:51:24 -0300

    adding comments and test cases

--- secret-buddy-buildpack_test.go
+++ secret-buddy-buildpack_test.go
@@ -5,6 +5,8 @@ import (
 	"testing"
 )
 
+// # This will set the KEY1 env var to a comma-delimited list of current and previous values:
+// heroku config:set HEROKU_SECRETS_CONFIG='{"KEY1": "current.KEY1,previous.KEY1"}'
 func TestExportEnvVarsFromMap(t *testing.T) {
 	// Define a test case
 	testCase := struct {
@@ -18,6 +20,108 @@ func TestExportEnvVarsFromMap(t *testing.T) {
 		},
 		expectedOutput: map[string]string{
 			"DB_PASSWORD": "current_password,previous_password",
+		},
+	}
+
+	// Call the ExportEnvVarsFromMap function with the test case input
+	outputValue, err := ExportEnvVarsFromMap(testCase.inputEnv, testCase.inputRules)
+
+	// Check if there was an error
+	if err != nil {
+		t.Errorf("Unexpected error: %v", err)
+	}
+
+	// Check if the output matches the expected output
+	for key, value := range testCase.expectedOutput {
+		if outputValue[key] != value {
+			t.Errorf("Expected %s but got %s for key %s", value, outputValue[key], key)
+		}
+	}
+}
+
+// # This will use the previous version of the KEY1 secret for the KEY1 env var:
+// heroku config:set HEROKU_SECRETS_CONFIG='{"KEY1": "previous.KEY1"}'
+func TestExportPreviousEnvVarsFromMap(t *testing.T) {
+	// Define a test case
+	testCase := struct {
+		inputEnv       string
+		inputRules     map[string]string
+		expectedOutput map[string]string
+	}{
+		inputEnv: `{"current": {"DB_PASSWORD": "current_password"}, "previous": {"DB_PASSWORD": "previous_password"}}`,
+		inputRules: map[string]string{
+			"DB_PASSWORD": "previous.DB_PASSWORD",
+		},
+		expectedOutput: map[string]string{
+			"DB_PASSWORD": "previous_password",
+		},
+	}
+
+	// Call the ExportEnvVarsFromMap function with the test case input
+	outputValue, err := ExportEnvVarsFromMap(testCase.inputEnv, testCase.inputRules)
+
+	// Check if there was an error
+	if err != nil {
+		t.Errorf("Unexpected error: %v", err)
+	}
+
+	// Check if the output matches the expected output
+	for key, value := range testCase.expectedOutput {
+		if outputValue[key] != value {
+			t.Errorf("Expected %s but got %s for key %s", value, outputValue[key], key)
+		}
+	}
+}
+
+// # This will set KEY1 to the current value, and another secret to the previous value.
+// # (The default still applies to KEY1, although you could be explicit if you want):
+func TestExportAlternatePreviousEnvVarsFromMap(t *testing.T) {
+	// Define a test case
+	testCase := struct {
+		inputEnv       string
+		inputRules     map[string]string
+		expectedOutput map[string]string
+	}{
+		inputEnv: `{"current": {"DB_PASSWORD": "current_password"}, "previous": {"DB_PASSWORD": "previous_password"}}`,
+		inputRules: map[string]string{
+			"OTHER_DB_PASSWORD": "previous.DB_PASSWORD",
+		},
+		expectedOutput: map[string]string{
+			"OTHER_DB_PASSWORD": "previous_password",
+		},
+	}
+
+	// Call the ExportEnvVarsFromMap function with the test case input
+	outputValue, err := ExportEnvVarsFromMap(testCase.inputEnv, testCase.inputRules)
+
+	// Check if there was an error
+	if err != nil {
+		t.Errorf("Unexpected error: %v", err)
+	}
+
+	// Check if the output matches the expected output
+	for key, value := range testCase.expectedOutput {
+		if outputValue[key] != value {
+			t.Errorf("Expected %s but got %s for key %s", value, outputValue[key], key)
+		}
+	}
+}
+
+// # This will set KEY1 to a JSON-encoded list including current and previous values:
+// heroku config:set HEROKU_SECRETS_CONFIG='{"KEY1": "[\"current.KEY1\",\"previous.KEY1\"]"}
+func TestExportJSONEnvVarsFromMap(t *testing.T) {
+	// Define a test case
+	testCase := struct {
+		inputEnv       string
+		inputRules     map[string]string
+		expectedOutput map[string]string
+	}{
+		inputEnv: `{"current": {"DB_PASSWORD": "current_password"}, "previous": {"DB_PASSWORD": "previous_password"}}`,
+		inputRules: map[string]string{
+			"DB_PASSWORD": "[\"current.DB_PASSWORD\", \"previous.DB_PASSWORD\"]",
+		},
+		expectedOutput: map[string]string{
+			"DB_PASSWORD": "current_password",
 		},
 	}
 
